a428e83de7e2f40bed9dfe4f5cce5db8300e3e02
Eric O'Callaghan <eric@ericoc.com>
2024-08-11 10:48:17 -0400

    working 404?

--- app.py
+++ app.py
@@ -1,44 +1,18 @@
-from flask import Flask, flash, render_template
+from flask import Flask, render_template
 
 
-# Flask.
 app = Flask(__name__)
 app.config.from_pyfile("config.py")
 
 
-def _error(
-    message="Sorry, but there was an unknown error.",
-    category="warn",
-    code=500
-):
-    """Error handler."""
-    flash(message, category)
-    return render_template("index.html.j2"), code or 500
-
-
-@app.errorhandler(404)
-def _page_not_found(message):
-    """Page Not Found (404 error handler)."""
-    return _error(message=message, code=404)
-
-
-@app.before_request
-def start():
-    """Start."""
-    pass
-
-
 @app.route("/", methods=["GET"])
 def index():
-    """Index."""
     return render_template("index.html.j2")
 
 
-@app.teardown_appcontext
-def end(err=None):
-    """End."""
-    if err:
-        raise err
+@app.errorhandler(404)
+def not_found(e):
+    return render_template("404.html.j2"), 404
 
 
 if __name__ == "__main__":
